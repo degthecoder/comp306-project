d963ffe454e78eb06299621f851dc70af7789863
Duha Emir Ganioglu <deg@DEGBookPro.local>
2023-06-11 20:56:54 +0300

    Axios added

--- front-end/src/api/simple.js
+++ front-end/src/api/simple.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const url = 'http://localhost:3000';
 
-export const fetchLogin = (userData) => {
+export const = (data) => {
     // eslint-disable-next-line no-console
-    const authenticate = axios.post(`${url}/`, userData);
+    const authenticate = axios.post(`${url}/`, data);
     
     return authenticate;
 };
